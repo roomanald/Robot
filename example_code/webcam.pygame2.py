6aa11adb775a48d48b5627aea9fb545f4cc7724a
roomanald <ronnie.day@hotmail.co.uk>
2015-03-09 12:56:14 +0000

    Update webcam.pygame2.py

--- example_code/webcam.pygame2.py
+++ example_code/webcam.pygame2.py
@@ -15,32 +15,29 @@ count = 0
 width = 1024
 height = 860
 
-while count < 5:
+while True:
    cam = pygame.camera.Camera("/dev/video0",(width,height))
    cam.start()
    image = cam.get_image()
    cam.stop()
-   pygame.image.save(image,str(count ) +'.jpg')
-   time.sleep(1) 
+   fileName = str(count ) +'.jpg'
+   pygame.image.save(image,fileName)
+   print("finished taking photo " + fileName)
+
+   msg = MIMEMultipart()
+   msg.attach(MIMEImage(file(fileName).read()))
+
+   print("read file")
+   # to send
+   try:
+      s = smtplib.SMTP('smtp.gmail.com:587')
+      s.ehlo()
+      s.starttls()
+      s.login('ronnie.day1@gmail.com','couxL2G3')
+      s.sendmail('ronnie.day@rbccm.com',['ronnie.day@rbccm.com'], msg.as_string())
+      s.quit()
+      print("Successfully sent email")
+   except:
+      print("Unexpected error:", sys.exc_info()[0])
+   time.sleep(60) 
    count = count + 1
-
-print("finished taking photos")
-
-msg = MIMEMultipart()
-msg.attach(MIMEImage(file("0.jpg").read()))
-
-print("read file")
-
-# to send
-try:
-   s = smtplib.SMTP('smtp.gmail.com:587')
-   s.ehlo()
-   s.starttls()
-   s.login('ronnie.day1@gmail.com','couxL2G3')
-   s.sendmail('ronnie.day@rbccm.com',['ronnie.day@rbccm.com'], msg.as_string())
-   s.quit()
-   print("Successfully sent email")
-except:
-    print("Unexpected error:", sys.exc_info()[0])
-
-
