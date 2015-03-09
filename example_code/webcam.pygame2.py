2b7c610aed496932ccc87460df7b4c0af5d0f284
roomanald <ronnie.day@hotmail.co.uk>
2015-03-09 22:09:01 +0000

    Update webcam.pygame2.py

--- example_code/webcam.pygame2.py
+++ example_code/webcam.pygame2.py
@@ -34,8 +34,8 @@ while True:
       continue;
       
       
-   i1 = ImageOps.posterize(ImageOps.equalize(ImageOps.autocontrast(Image.open(fileName).convert("L")))).histogram()
-   i2 = ImageOps.posterize(ImageOps.equalize(ImageOps.autocontrast(Image.open(previousFileName).convert("L")))).histogram()
+   i1 = ImageOps.posterize(ImageOps.equalize(ImageOps.autocontrast(Image.open(fileName).convert("L"))),2).histogram()
+   i2 = ImageOps.posterize(ImageOps.equalize(ImageOps.autocontrast(Image.open(previousFileName).convert("L"))),2).histogram()
    
 
    # calculate rms
