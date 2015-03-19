1019b7b2c9ce8d7cc16cded6c667e98aee2aa376
roomanald <ronnie.day@hotmail.co.uk>
2015-03-19 17:55:23 +0000

    Update webcam2.py

--- example_code/webcam2.py
+++ example_code/webcam2.py
@@ -35,7 +35,7 @@ class App():
 		pygame.image.save(background, "background.jpg")
 		pygame.image.save(thresholded, "thresholded.jpg")
 		msg = MIMEMultipart()
-		msg['Subject'] = 'Intruder - (' + diffAmount + ')' 
+		msg['Subject'] = 'Intruder - (' + str(diffAmount) + ')' 
 		msg.attach(MIMEImage(file("image.jpg").read(),name=os.path.basename(image1)))
 		msg.attach(MIMEImage(file("background.jpg").read(),name=os.path.basename(image2)))
 		msg.attach(MIMEImage(file("thresholded.jpg").read(),name=os.path.basename(image3)))
