831dc4ce61524e0ee47eaf811f3004ea750499ec
roomanald <ronnie.day@hotmail.co.uk>
2015-03-19 13:47:53 +0000

    Update pyaudioexample.py

--- example_code/pyaudioexample.py
+++ example_code/pyaudioexample.py
@@ -6,12 +6,10 @@ import analyse
 pyaud = pyaudio.PyAudio()
 
 # Open input stream, 16-bit mono at 44100 Hz
-# On my system, device 4 is a USB microphone
 stream = pyaud.open(
     format = pyaudio.paInt16,
     channels = 1,
     rate = 44100,
-    input_device_index = 2,
     input = True)
 
 while True:
