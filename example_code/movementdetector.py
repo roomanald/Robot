91c11d46e1e523dd202b08a51eb40980ae2b2066
roomanald <ronnie.day@hotmail.co.uk>
2015-03-20 10:44:54 +0000

    Update movementdetector.py

--- example_code/movementdetector.py
+++ example_code/movementdetector.py
@@ -1,1 +1,44 @@
+import pygame
+from pygame.locals import *
+import pygame.camera
+import logging
 
+class MovementDetector:
+    def __init__(self, logger, movementCallback, sampleCount, pixelDiffThreshold, imageSize):
+        self.logger = logger
+        self.movementCallback = movementCallback
+        self.sampleCount = sampleCount
+        self.pixelDiffThreshold = pixelDiffThreshold
+	    self.imageSize = imageSize
+	    self.bg = []
+	    self.cam = pygame.camera.Camera("/dev/video0",size)
+	
+	def start(self)
+	
+	    thresholded = pygame.surface.Surface(self.imageSize)
+	    
+		self.cam.start()
+		
+        while True:
+            image = self.cam.get_image()
+		    if (len(self.bg) != self.sampleCount):
+			    self.bg.append(image)
+			    self.logger.debug("filling image buffer count=" + str(len(self.bg)))
+			    continue;
+			    
+	        background = pygame.transform.average_surfaces(self.bg)
+	        
+	        similarPixels = pygame.transform.threshold(thresholded,image,(0,255,0),(30,30,30),(0,0,0),1,background)
+	        
+			diff = self.imageSize[0]*self.imageSize[1] - similarPixels
+			isDiff = diff > self.pixelDiffThreshold
+			
+			if (isDiff):
+			    movementCallback(image, background, thresholded, diff)
+	        
+	        self.bg.pop(0)
+	        self.bg.append(image)
+	        
+	def stop(self)
+	    self.cam.stop()
+	    
