9ce65405f39b7817344a4da132ddee111e4160c5
roomanald <ronnie.day@hotmail.co.uk>
2015-03-20 10:48:13 +0000

    Create pygameHelper.py

---
+++ example_code/pygameHelper.py
@@ -0,0 +1,11 @@
+from PIL import Image, ImageChops, ImageOps
+import cStringIO
+import pygame
+from pygame.locals import *
+
+def convertPygameSurfaceToMemoryStream(surface, imageSize)
+    imstring = pygame.image.tostring(surface,"RGB")
+	im = Image.fromstring("RGB", imageSize, imstring)
+	memf = cStringIO.StringIO()
+	im.save(memf, "JPEG")
+	return memf
