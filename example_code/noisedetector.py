d2ab653f3dc3b8f34ca2db7cc490c17a2c40eb6a
roomanald <ronnie.day@hotmail.co.uk>
2015-03-20 10:59:38 +0000

    Update noisedetector.py

--- example_code/noisedetector.py
+++ example_code/noisedetector.py
@@ -5,55 +5,51 @@ import analyse
 import logging
 
 class NoiseDetector:
-    def __init__(self, logger, noiseCallback, sampleCount, threshold):
-        self.logger = logger
-        self.noiseCallback = noiseCallback
-        self.sampleCount = sampleCount
-        self.threshold = threshold
-	      self.bg = []
-	      self.sampleRate = int(pyaud.get_device_info_by_index(0)['defaultSampleRate'])
-	      self.pyaud = pyaudio.PyAudio()
+	def __init__(self, logger, noiseCallback, sampleCount, threshold):
+		self.logger = logger
+		self.noiseCallback = noiseCallback
+		self.sampleCount = sampleCount
+		self.threshold = threshold
+		self.bg = []
+		self.sampleRate = int(pyaud.get_device_info_by_index(0)['defaultSampleRate'])
+		self.pyaud = pyaudio.PyAudio()
 	      
 	def start(self)
-	  # Open input stream, 16-bit mono at 44100 Hz
-		  stream = self.pyaud.open(
-  			format = pyaudio.paInt16,
-  			channels = 1,
-  			rate = self.sampleRate,
-  			input = True)
+		# Open input stream, 16-bit mono at 44100 Hz
+		stream = self.pyaud.open(format = pyaudio.paInt16,channels = 1,	rate = self.sampleRate,	input = True)
 		
         while True:
           
-          try:
-				    # Read raw microphone data
-				    rawsamps = stream.read(8192)
-				    # Convert raw data to NumPy array
-				    samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
-				    # Show the volume and pitch
-				    volume = analyse.loudness(samps)#, analyse.musical_detect_pitch(samps)
-			    except IOError: 
-      				self.logger.debug("dropped mic frame")
+		try:
+			# Read raw microphone data
+			rawsamps = stream.read(8192)
+			# Convert raw data to NumPy array
+			samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+			# Show the volume and pitch
+			volume = analyse.loudness(samps)#, analyse.musical_detect_pitch(samps)
+		except IOError: 
+			self.logger.debug("dropped mic frame")
       				
-		      if (len(self.bg) != self.sampleCount):
-			      self.bg.append(volume)
-			      self.logger.debug("filling noise buffer count=" + str(len(self.bg)))
-			      continue;
+		if (len(self.bg) != self.sampleCount):
+			self.bg.append(volume)
+			self.logger.debug("filling noise buffer count=" + str(len(self.bg)))
+			continue;
 			    
 	        averageLoudness = sum(self.bg) / float(len(self.bg))
 	        diff = abs(volume) - abs(averageLoudness)
-			    isDiff = diff > self.threshold
-			    self.logger.debug("volume diff " + str(diff))
-			
-			    if (isDiff):
-			      noiseCallback(diff, averageLoudness)
+		isDiff = diff > self.threshold
+		self.logger.debug("volume diff " + str(diff))
+		
+		if (isDiff):
+			noiseCallback(diff, averageLoudness)
 	        
 	        self.bg.pop(0)
 	        self.bg.append(volume)
 	        
 	def stop(self)
-	    # stop stream (4)
-      self.stream.stop_stream()
-      self.stream.close()
-
-      # close PyAudio (5)
-      self.pyaud.terminate()
+		# stop stream (4)
+		self.stream.stop_stream()
+		self.stream.close()
+		
+		# close PyAudio (5)
+		self.pyaud.terminate()
